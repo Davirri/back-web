3dd13900c30fd445da47ff0a4b597194fc1b15b9
Davirri <167114229+Davirri@users.noreply.github.com>
2024-11-01 21:57:55 +0100

    vercel

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.use(express.json());
 
 app.use('/', routes); // Usamos las rutas definidas en el archivo routes.js
 
+// Middleware para manejar rutas no encontradas
+app.use((req, res) => {
+  res.status(404).send('Cannot GET /');
+});
+
 const PORT = process.env.PORT || 5000; // Establecemos el puerto desde .env o usa 5000 por defecto
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`); // Iniciamos el servidor y muestra el puerto en consola
