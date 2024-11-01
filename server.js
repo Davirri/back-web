4a7d9162faaaaa5e3a2d1bbf30bd109959763d15
Davirri <167114229+Davirri@users.noreply.github.com>
2024-11-01 22:10:41 +0100

    vercel otra vez

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 const express = require('express'); 
 const cors = require('cors'); 
-const dotenv = require('dotenv'); // Importamos dotenv para manejar variables de entorno
+const dotenv = require('dotenv'); 
 const routes = require('./routes/routes'); 
 
 dotenv.config(); 
 const app = express(); 
 
+// Middleware
 app.use(cors()); 
 app.use(express.json()); 
 
+// Rutas
 app.use('/', routes); // Usamos las rutas definidas en el archivo routes.js
 
 // Middleware para manejar rutas no encontradas
 app.use((req, res) => {
-  res.status(404).send('Cannot GET /');
+  res.status(404).send('Cannot GET ' + req.originalUrl);
+});
+
+// Manejo de errores
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 const PORT = process.env.PORT || 5000; // Establecemos el puerto desde .env o usa 5000 por defecto
